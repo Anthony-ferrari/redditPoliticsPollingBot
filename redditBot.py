f762c74aac12d36eef8176b43313845c5d77ccfc
Anthony-ferrari <anthonyferrari1611@gmail.com>
2020-07-05 13:24:17 -0700

    formatted response with data collected in webscrape538.py

--- redditBot.py
+++ redditBot.py
@@ -1,12 +1,11 @@
 import praw
-
+from webscrape538 import fivethirtyeight
 #reddit api login
-reddit = praw.Reddit(client_id = 'k_jOB-RRBSIsxA',
-                    client_secret = '	IAqcUz409vr9IIZNmzoOlaj_zG0',
+reddit = praw.Reddit(client_id = "k_jOB-RRBSIsxA",
+                    client_secret = "IAqcUz409vr9IIZNmzoOlaj_zG0",
                     username = 'amal25',
                     password = 'newredditbotPolls',
                     user_agent = 'pollsbot by /u/amal25')
-
 # the subreddits you want your bot to live on
 subreddit = reddit.subreddit('politics')
 
@@ -14,19 +13,26 @@ subreddit = reddit.subreddit('politics')
 keyphrase = '!todaysPoll'
 
 # looks for phrase and replies accordingly
-for comment in subreddit.steam.comments():
+for comment in subreddit.stream.comments():
     if keyphrase in comment.body:
         word = comment.body.replace(keyphrase, '')
-        try:
-            # we get the data from another python file (webscraping)
-            theReply = "| Dates | Pollster | Sample | Approval | Results | Disapproval | Net Results \n"
-            spacing = ":-- | :--: | :--: | :--: | :--: | :--: | :--: | --: \n"
-            
+        # we get the data from another python file (webscraping)
+        if fivethirtyeight():
+            theReply = "| Poll Name| Dates | Pollster | Sample | Sample Type | Approval | Disapproval | Net Results |\n"
+            spacing = "| :-- | :--: | :--: | :--: | :--: | :--: | :--: | --: |\n"
+            endComment = theReply + spacing
+            polls = fivethirtyeight()
+            for i in polls:
+                endComment += i
             # the below is the format for reply
-            comment.reply('')
-            print('posted')
-        except:
-            print("You are requesting this too frequently.")
+            comment.reply(endComment)
+            print('posted the poll!')
+        else:
+            reply = "no polls at this time"
+            comment.reply(reply)
+            print('posted - no content')
+        # except:
+        #     print("You are requesting this too frequently.")
 
 
 
--- webscrape538.py
+++ webscrape538.py
@@ -1,20 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
+def fivethirtyeight():
+    # get the data
+    data = requests.get('https://projects.fivethirtyeight.com/polls/')
 
-# get the data
-data = requests.get('https://projects.fivethirtyeight.com/polls/')
+    # load data into bs4
+    soup = BeautifulSoup(data.text, 'html.parser')
 
-# load data into bs4
-soup = BeautifulSoup(data.text, 'html.parser')
+    # create list for items 
+    pollList = []
 
-# get data by looking for table 
-div = soup.find('div', { 'class': 'day-container'})
-dateAdded = div.find('h2', {'class': 'day'})
-pollsTbody =div.find('tbody')
+    # get data by looking for table 
+    div = soup.find('div', { 'class': 'day-container'})
+    dateAdded = div.find('h2', {'class': 'day'})
+    pollsTbody =div.find('tbody')
 
-for tr in pollsTbody.findall('tr', {'class': 'new visible-row'}):
-    pollName = tr.findall('td',{ 'class', 'type hide-mobile'}).find_all('a')[0].text.strip()
-    
+    newString = ""
+
+    for tr in pollsTbody.find_all('tr', {'class': 'visible-row'}):
+        pollName = tr.find_all('td',{ 'class', 'type'})[0].text.strip()
+
+        datePosted = tr.find_all('td', {'class','dates hide-mobile'})[0].find_all('div')[0].text.strip()
+
+        pollster = tr.find_all('td', {'class', 'pollster'})[0].find_all('a')[1].text.strip()
+
+        sampleSize = tr.find_all('td', {'class', 'sample'})[0].text.strip()
+
+        sampleSizeType = tr.find_all('td', {'class', 'sample-type'})[0].text.strip()
+
+        approvePercent = tr.find_all('td', {'class', 'value'})[0].find_all('div')[0].text.strip()
+
+        disapprovePercent = tr.find_all('td', {'class', 'value'})[1].find_all('div')[0].text.strip()
+
+        netResultName = tr.find_all('td', {'class', 'leader'})[0].text.strip()
+        netResultValue = tr.find_all('td', {'class', 'net'})[0].text.strip()
+        netResult = netResultName + " " + netResultValue
+
+
+        newString += " | " + pollName + " | " + datePosted + " | " + pollster + " | " + sampleSize + " | " + sampleSizeType + " | " + approvePercent + " | " + disapprovePercent + " | " + netResult + " | \n"
+        pollList.append(newString)
+    return pollList
+
+fivethirtyeight()
 
 # classes that matter:
 
@@ -40,7 +67,5 @@ for tr in pollsTbody.findall('tr', {'class': 'new visible-row'}):
 #### net result block
 # leader hide-mobile
 # net hide-mobile disapprove 
-
-print(table)
 
     
